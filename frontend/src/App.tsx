e6aa730fbbd11ec79ef0afd3b43dbdbb90a5f457
E-Holt <emma.d.holt@gmail.com>
2024-08-13 10:54:14 +1000

    frontend fix

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Em's Stuff and Things!" />
+    </div>
+  );
+}
+
+export default App;
